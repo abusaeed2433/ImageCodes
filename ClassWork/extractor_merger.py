36a04f076a6bbafa5e60de39787da83f0c974d4b
Abu Saeed <abusaeed2433@gmail.com>
2024-02-21 02:01:44 +0600

    hsv added, but not working

--- ClassWork/extractor_merger.py
+++ ClassWork/extractor_merger.py
@@ -9,6 +9,19 @@ def extract_rgb(image):
 def merge_rgb( red, green, blue ):
     return cv2.merge( [blue, green, red] )
 
+def extract_hsv(image):
+    h_channel, s_channel, v_channel = cv2.split(image)
+    return (h_channel, s_channel, v_channel)
+
+def merge_hsv(h, s, v):
+    return cv2.merge([h, s, v])
+
+def hsv_to_rgb(hsv):
+    return cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+    #return cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+
+def rgb_to_hsv(rgb):
+    return cv2.cvtColor(rgb, cv2.COLOR_BGR2HSV)
 
 # image_path = 'ClassWork/image_girl.jpg'
 # image = cv2.imread(image_path)
---
+++ ClassWork/hsv_convolve.py
@@ -0,0 +1,62 @@
+import cv2
+import numpy as np
+from kernal_generator import *
+
+from extractor_merger import extract_hsv
+from extractor_merger import merge_hsv
+from extractor_merger import hsv_to_rgb
+from extractor_merger import rgb_to_hsv
+
+from convolution import normalize
+from convolution import convolve
+
+def convolve_hsv(image, kernel, kernel_center=(-1,-1)):
+    image = rgb_to_hsv(image)
+    hue, sat, val = extract_hsv(image=image)
+
+    hue_conv = convolve(image=hue, kernel=kernel, kernel_center=kernel_center)
+    hue_nor = normalize(hue_conv)
+    
+    sat_conv = convolve(image=sat, kernel=kernel, kernel_center=kernel_center)
+    sat_nor = normalize(sat_conv)
+    
+    val_conv = convolve(image=val, kernel=kernel, kernel_center=kernel_center)
+    val_nor = normalize(val_conv)
+    
+    cv2.imshow("Extracted Hue", hue_nor)
+    cv2.imshow("Extracted Sat", sat_nor)
+    cv2.imshow("Extracted Val", val_nor)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    
+    merged_hsv = merge_hsv(h=hue_nor, s=sat_nor, v=val_nor)
+    #merged_rgb = hsv_to_rgb(merge_hsv)
+    
+    merged_rgb = cv2.cvtColor(merged_hsv, cv2.COLOR_HSV2BGR)
+    orignial_hsv = image
+    orignial_rgb = cv2.cvtColor(image, cv2.COLOR_HSV2BGR)
+    
+    cv2.imshow("Original(HSV) image", orignial_hsv)
+    cv2.imshow("Merged(HSV) image", merged_hsv)
+    cv2.waitKey(0)
+    
+    cv2.destroyAllWindows()
+
+
+image_path = 'ClassWork\\image_girl.jpg'
+image = cv2.imread( image_path )
+
+kernel = np.array([
+        [0, 0, 0],
+        [0, 1, 0],
+        [0, 0, 0]
+    ])
+# kernel = np.array([
+#     [0.00291502, 0.0130642,  0.02153923, 0.0130642,  0.00291502],
+#     [0.0130642,  0.05854969, 0.09653213, 0.05854969, 0.0130642 ],
+#     [0.02153923, 0.09653213, 0.15915457, 0.09653213, 0.02153923],
+#     [0.0130642,  0.05854969, 0.09653213, 0.05854969, 0.0130642 ],
+#     [0.00291502, 0.0130642,  0.02153923, 0.0130642,  0.00291502]
+# ])
+
+convolve_hsv(image=image, kernel=kernel, kernel_center=(-1,-1))
