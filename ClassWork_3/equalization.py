b6f4151e3de53f848e8e765e758b6a885cba1b69
Abu Saeed <abusaeed2433@gmail.com>
2024-04-05 21:38:31 +0600

    project base done

--- ClassWork_3/equalization.py
+++ ClassWork_3/equalization.py
@@ -7,7 +7,7 @@ def plot(data,figure):
     plt.plot(data)
     plt.show()
     
-def show_all(image, c):
+def show_all(image, c, show=True):
     hist = cv2.calcHist([image], [0], None, [256],[0,256])
     
     dum = []
@@ -22,24 +22,27 @@ def show_all(image, c):
     
     total = np.sum(hist)
     
-    cv2.imshow("Image",image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    if show:
+        cv2.imshow("Image",image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
     
     #print(len(hist))
     pdf = hist.copy()
     for i in range(len(hist)):
         pdf[i] = hist[i] / float(total)
 
-    plot(data=hist,figure=c+1)
-    plot(data=pdf,figure=c+2)
+    if show:
+        plot(data=hist,figure=c+1)
+        plot(data=pdf,figure=c+2)
     
     cdf = pdf.copy()
     
     for i in range(1,len(pdf)):
         cdf[i] = pdf[i] + cdf[i-1]
     
-    plot(cdf,c+3)
+    if show:
+        plot(cdf,c+3)
     
     in_map = cdf.copy()
     for i in range(len(cdf)):
@@ -47,9 +50,9 @@ def show_all(image, c):
     
     return hist, pdf, cdf, in_map
 
-def start(image):
+def equalize(image, show = True):
     
-    hist, pdf, cdf, in_map = show_all(image=image,c = 1)
+    hist, pdf, cdf, in_map = show_all(image=image,c = 1, show=show)
     
     h,w = image.shape
     
@@ -59,17 +62,18 @@ def start(image):
             px_val = image[x,y]
             out[x,y] = in_map[ px_val ]
     
-    _,_,_,_ = show_all(out,c=5)
-    
-    cv2.destroyAllWindows()
-    
-    cv2.imshow("Output",out)
-    img2 = cv2.equalizeHist(image)
-    cv2.imshow("Output_library",img2)
-    cv2.waitKey(0)
-    
-    cv2.destroyAllWindows()
+    if show:
+        _,_,_,_ = show_all(out,c=5,show=show)
+        cv2.destroyAllWindows()
+        
+        cv2.imshow("Output",out)
+        img2 = cv2.equalizeHist(image)
+        cv2.imshow("Output_library",img2)
+        cv2.waitKey(0)
+        
+        cv2.destroyAllWindows()
+    return out
 
 
-image = cv2.imread( "./images\\histogram.jpg", cv2.IMREAD_GRAYSCALE)
-start(image=image)
+# image = cv2.imread( "ClassWork_3\\images\\histogram.jpg", cv2.IMREAD_GRAYSCALE)
+# equalize(image=image, show=False)
